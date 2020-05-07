6db2e2017afd83c8542e276fb0c03f1a7bd2b8a9
Thanh Nguyen <thanhthanchuc@gmail.com>
2020-05-07 19:38:09 +0700

    init commit

---
+++ Chart.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Chart
+{
+    partial class Chart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.student_combobox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.student_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.student_chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // student_combobox
+            // 
+            this.student_combobox.FormattingEnabled = true;
+            this.student_combobox.Location = new System.Drawing.Point(83, 61);
+            this.student_combobox.Name = "student_combobox";
+            this.student_combobox.Size = new System.Drawing.Size(121, 21);
+            this.student_combobox.TabIndex = 1;
+            this.student_combobox.SelectedIndexChanged += new System.EventHandler(this.student_combobox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chọn sinh viên:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // student_chart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.student_chart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.student_chart.Legends.Add(legend4);
+            this.student_chart.Location = new System.Drawing.Point(37, 111);
+            this.student_chart.Name = "student_chart";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.student_chart.Series.Add(series4);
+            this.student_chart.Size = new System.Drawing.Size(719, 327);
+            this.student_chart.TabIndex = 3;
+            this.student_chart.Text = "chart1";
+            // 
+            // Chart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.student_chart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.student_combobox);
+            this.Name = "Chart";
+            this.Text = "Chart";
+            this.Load += new System.EventHandler(this.Chart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.student_chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox student_combobox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart student_chart;
+    }
+}
+
---
+++ Chart.cs
@@ -0,0 +1,94 @@
+﻿using Chart.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Chart
+{
+    public partial class Chart : Form
+    {
+        private readonly string _fullPath = "..\\..\\CSVs\\Student.csv";
+
+        private List<Student> students = new List<Student>();
+
+        private List<string> label = new List<string> { "Math Point", "Physical Point", "Chemistry Point" };
+
+        private Series _series;
+
+        public Chart()
+        {
+            InitializeComponent();
+        }
+
+        private void Chart_Load(object sender, EventArgs e)
+        {
+            students = _getStudents(_fullPath);
+
+            student_combobox.DropDownStyle = ComboBoxStyle.DropDown;
+            student_combobox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            student_combobox.AutoCompleteSource = AutoCompleteSource.ListItems;
+            student_combobox.ValueMember = "Id";
+            student_combobox.DisplayMember = "Name";
+
+            students.ForEach(st =>student_combobox.Items.Add(st));
+
+            student_chart.Series.Clear();
+            label.ForEach(str => {
+                _series = student_chart.Series.Add(str);
+                _series.Points.Add(0);
+            });
+        }
+
+        private List<Student> _getStudents(string csvPath)
+        {
+            List<string[]> rows = File.ReadAllLines(csvPath).Select(x => x.Split(',')).ToList();
+            List<Student> students = new List<Student>();
+
+            int rowLength = rows.Count();
+            for(int i = 1; i < rowLength; i++)
+            {
+                string[] vals = rows[i];
+
+                Student student = new Student();
+
+                student.Id = int.Parse(vals[0]);
+                student.Name = vals[2] + vals[3];
+                student.Age = DateTime.Now.Year - DateTime.Parse(vals[12]).Year;
+                student.MathPoint = int.Parse(vals[14]);
+                student.PhysicalPoint = int.Parse(vals[15]);
+                student.ChemistryPoint = int.Parse(vals[16]);
+
+                students.Add(student);
+            }
+
+            return students;
+        }
+
+        private void student_combobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+
+            Student selectedStudent = (Student) comboBox.SelectedItem;
+
+            Student student = students.Where(st => st.Id == selectedStudent.Id).ToArray()[0];
+
+            Draw(student);
+        }
+
+        private void Draw(Student student = null)
+        {
+            student_chart.Series["Math Point"].Points.ElementAt(0).XValue = student.MathPoint;
+            student_chart.Series["Physical Point"].Points.ElementAt(0).SetValueY(student.PhysicalPoint);
+            student_chart.Series["Chemistry Point"].Points.ElementAt(0).SetValueY(student.ChemistryPoint);
+            student_chart.Refresh();
+        }
+    }
+}
